e76c2b3d08fdfc20b9a6788500e8c964e38c3fe6
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-11-24 20:19:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+   int c=0,a=1,b=1,c=0;
+   cin>>n;
+   if(n<2){
+      cout<<"errore"<<endl;
+   }
+   else{
+      for(int i=0;i<n;i++);
+      cout<<a<<endl;
+      c=a+b;
+      a=b;
+      b=c;
+   }
    return 0;
 }
